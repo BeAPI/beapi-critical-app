d5fb3418ee02ec98a6b97224f6eb4f5f7d0ed9cf
Milan Ricoul <mricoul@beapi.fr>
2018-09-07 12:11:20 +0200

    Separate Electron and Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,11 @@ gulp.task('scripts', getTask('scripts'))
 // Styles
 gulp.task('sass', getTask('sass'))
 
-// Electron
-gulp.task('electron', getTask('electron'))
-
 // Gulp Assets Build
 gulp.task('build', ['scripts', 'sass'])
 
 // Gulp Default Task
 gulp.task('default', ['scripts', 'sass'], function () {
-  gulp.watch('app/*', ['electron'])
   gulp.watch('app/js/*.js', ['scripts'])
   gulp.watch(['app/css/*.scss', 'app/css/**/*.scss'], ['sass'])
 });
--- tasks/electron.js
+++
@@ -1,8 +0,0 @@
-runElectron = require('gulp-run-electron')
-
-module.exports = function (gulp) {
-  return function () {
-    gulp.src('app')
-      .pipe(runElectron(["--cli-argument", "--another"], { cwd: "path" }))
-  }
-}
