f0911389a47e4e69c4afca15b992ed7aba7a1baa
Milan Ricoul <mricoul@beapi.fr>
2018-10-11 16:38:40 +0200

    remove app folder

--- index.js
+++ index.js
@@ -2,44 +2,40 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev')
 
+let win;
+
 function createWindow() {
-  // Create Browser Window
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1280,
     height: 720,
-    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      preload: __dirname + '/preload.js'
+    }
   })
 
   win.setPosition(0, 0)
 
-  // Load view
-  if (isDev) {
-    win.loadURL('http://localhost:3000')
-  } else {
-    win.loadURL('http://localhost:3000')
-  }
-
-  win.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+  win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+      app.quit()
+  }
+});
+
+app.on('activate', function () {
+  if (win === null) {
+      createWindow()
+  }
+});
